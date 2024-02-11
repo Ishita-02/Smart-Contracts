e55d524608d1ddbc094731196d42f11b9be3982f
Ishita-02 <88130678+Ishita-02@users.noreply.github.com>
2024-02-11 14:45:00 +0530

    Update Escrow.sol

--- Escrow.sol
+++ Escrow.sol
@@ -33,4 +33,40 @@ contract Escrow {
         arbiter = _arbiter;
         state = State.Created;
     }
+
+    function deposit() external payable onlyBuyer inState(State.Created) {
+        emit FundsDeposited(msg.sender, address(this).balance);
+        state = State.Locked;
+    }
+
+    function releaseFunds() external onlyBuyer inState(State.Locked) {
+        emit FundsReleased(seller, address(this).balance);
+        payable(seller).transfer(address(this).balance);
+        state = State.Released;
+    }
+
+    function refundBuyer() external onlyBuyer inState(State.Locked) {
+        emit RefundInitiated(buyer, address(this).balance);
+        payable(buyer).transfer(address(this).balance);
+        state = State.Released;
+    }
+
+    function raiseDispute() external{
+        require(msg.sender == seller || msg.sender == buyer, "Only the buyer and seller can raise dispute");
+        state = State.InDispute;
+        emit DisputeRaised();
+    }
+
+    function resolveDispute(bool _buyerWins) external {
+        require(msg.sender == arbiter, " Only the arbiter can resolve dispute" );
+        if(_buyerWins) {
+            payable(buyer).transfer(address(this).balance);
+            emit RefundInitiated(buyer, address(this).balance);
+        }
+        else {
+            payable(seller).transfer(address(this).balance);
+            emit FundsReleased(seller, address(this).balance);
+        }
+        state = State.Released;
+    }
 }
